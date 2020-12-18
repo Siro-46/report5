40bf838e0faa5a1cdf185de74647f52cb82175a9
Takeya Akamine <e205753@ie.u-ryukyu.ac.jp>
2020-12-18 14:12:46 +0900

    1st commit

---
+++ jp/ac/uryukyu/ie/e205753/Report5.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e205753;
+
+public class Report5 {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+    
+}
