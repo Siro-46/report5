cb7efdf03d0a59e62f53117ca0790fb2dabfc3d9
Takeya Akamine <e205753@ie.u-ryukyu.ac.jp>
2020-12-22 14:47:52 +0900

    2nd commit

--- jp/ac/uryukyu/ie/e205753/Report5.java
+++ jp/ac/uryukyu/ie/e205753/Report5.java
@@ -3,6 +3,11 @@ package jp.ac.uryukyu.ie.e205753;
 public class Report5 {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しています。");
+            System.out.println(e.getMessage());
+        }
     }
 }
