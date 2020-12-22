a22d77872d602d18ee32f83f247b61344f102a15
Takeya Akamine <e205753@ie.u-ryukyu.ac.jp>
2020-12-22 20:37:04 +0900

    4th commit

--- jp/ac/uryukyu/ie/e205753/Report5.java
+++ jp/ac/uryukyu/ie/e205753/Report5.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e205753;
 
 public class Report5 {
     public static void main(String[] args) {
-        String str = "百二十二";
+        String str = "百二十三";
         int value = Integer.parseInt(str);
     }
 }
